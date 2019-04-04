6d1e4b26a30b242303850f63d9462bce014e154f
LeeQ19 <leekw1994@skku.edu>
2019-04-04 16:31:48 +0900

    make outlier list

--- Data.Statistic.R
+++ Data.Statistic.R
@@ -8,31 +8,34 @@ library("ggplot2")
 # Load data
 df.raw <- read.csv(url("http://bit.ly/Fire4Data"), header = T)
 
+# Set outlier
+id.out <- c(NA)
+df.clr <- df.raw[, ]
+
 #########################################################################################################################
 ### Data Statistic
 #########################################################################################################################
 
 # Employee - Reduction.of.damage
-ggplot(df.raw, aes(x = Employee, y = Reduction.of.damage, color = Location)) + 
+ggplot(df.clr, aes(x = Employee, y = Reduction.of.damage, color = Location)) + 
   geom_point()
 
 # Employee - Rescue
-ggplot(df.raw, aes(x = Employee, y = Rescue, color = Location)) + 
+ggplot(df.clr, aes(x = Employee, y = Rescue, color = Location)) + 
   geom_point()
 
 # Ambulance - Reduction.of.damage
-ggplot(df.raw, aes(x = Ambulance, y = Reduction.of.damage, color = Location)) + 
+ggplot(df.clr, aes(x = Ambulance, y = Reduction.of.damage, color = Location)) + 
   geom_point()
 
 # Ambulance - Rescue
-ggplot(df.raw, aes(x = Ambulance, y = Rescue, color = Location)) + 
+ggplot(df.clr, aes(x = Ambulance, y = Rescue, color = Location)) + 
   geom_point()
 
 # Firewagon - Reduction.of.damage
-ggplot(df.raw, aes(x = Firewagon, y = Reduction.of.damage, color = Location)) + 
+ggplot(df.clr, aes(x = Firewagon, y = Reduction.of.damage, color = Location)) + 
   geom_point()
 
 # Firewagon - Rescue
-ggplot(df.raw, aes(x = Firewagon, y = Rescue, color = Location)) + 
+ggplot(df.clr, aes(x = Firewagon, y = Rescue, color = Location)) + 
   geom_point()
-

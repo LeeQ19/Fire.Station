bfe9a063de4ab622aa54c8e6fd494eb0cb1222d9
LeeQ19 <leekw1994@skku.edu>
2019-04-19 10:55:15 +0900

    merge data statistics to Fire.Station.R

--- Fire.Station.R
+++ Fire.Station.R
@@ -2,6 +2,8 @@
 ### Setting up Environment
 #########################################################################################################################
 
+# Load library
+library("ggplot2")
 
 # Load function
 source("dm.dynamic.ba.R")
@@ -9,7 +11,10 @@ source("dm.dea.intertemporal.R")
 
 # Load data
 df.raw <- read.csv(url("http://bit.ly/Fire4Data"), header = T)
-df.eff <- simplify2array(by(df.raw[,-c(1, 11)], df.raw$Year, as.matrix))
+
+# Set outlier
+id.out <- c(12, 46, 80, 114, 148)
+df.eff <- simplify2array(by(df.raw[-id.out, -c(1, 11)], df.raw[-id.out, ]$Year, as.matrix))
 
 
 # Parameter
@@ -24,6 +29,41 @@ orientation <- "i"
 # Preprocess data
 df.final   <- apply(df.eff[, id.f, ], 1, sum)
 df.initial <- apply(df.eff[, id.z, ], 1, sum) + df.final
+
+#########################################################################################################################
+### Descriptive Statistics
+#########################################################################################################################
+
+# Summary
+boxplot(scale(df.raw[, 3:10]))
+summary(df.raw[, 3:10])
+
+boxplot(scale(df.raw[-id.out, 3:10]))
+summary(df.raw[-id.out, 3:10])
+
+# Employee - Reduction.of.damage
+ggplot(df.raw[-id.out, ], aes(x = Employee, y = Reduction.of.damage, color = Location)) + 
+  geom_point()
+
+# Employee - Rescue
+ggplot(df.raw[-id.out, ], aes(x = Employee, y = Rescue, color = Location)) + 
+  geom_point()
+
+# Ambulance - Reduction.of.damage
+ggplot(df.raw[-id.out, ], aes(x = Ambulance, y = Reduction.of.damage, color = Location)) + 
+  geom_point()
+
+# Ambulance - Rescue
+ggplot(df.raw[-id.out, ], aes(x = Ambulance, y = Rescue, color = Location)) + 
+  geom_point()
+
+# Firewagon - Reduction.of.damage
+ggplot(df.raw[-id.out, ], aes(x = Firewagon, y = Reduction.of.damage, color = Location)) + 
+  geom_point()
+
+# Firewagon - Rescue
+ggplot(df.raw[-id.out, ], aes(x = Firewagon, y = Rescue, color = Location)) + 
+  geom_point()
 
 #########################################################################################################################
 ### Analysis

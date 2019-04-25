b57e2ddfcedd3905b2e265527d048a4852806df4
LeeQ19 <leekw1994@skku.edu>
2019-04-26 01:58:36 +0900

    Bug fixed - available budget computation

--- Fire.Station.R
+++ Fire.Station.R
@@ -9,9 +9,8 @@ source("dm.dynamic.ba.R")
 source("dm.dea.intertemporal.R")
 
 # Load data
-df.2d  <- read.csv(url("http://bit.ly/Fire4Data"), header = T)
-df.eff  <- simplify2array(by(df.2d[, -c(1, 11)], df.2d$Year, as.matrix))
-
+df.2d <- read.csv(url("http://bit.ly/Fire4Data"), header = T)
+df.3d <- simplify2array(by(df.2d[, -c(1, 11)], df.2d$Year, as.matrix))
 
 # Parameter
 id.t <- c(1)
@@ -23,17 +22,38 @@ rts  <- "vrs"
 ori  <- "i"
 
 # Budget
-df.Z.T <- apply(df.eff[, id.f, ], 1, sum)
-df.Z.0 <- apply(df.eff[, id.z, ], 1, sum) + df.Z.T
+df.Z.T <- apply(df.3d[, id.f, ], 1, sum)
+df.Z.0 <- apply(df.3d[, id.z, ], 1, sum) + df.Z.T
 
 
 #########################################################################################################################
-### Descriptive Statistics
+### Analysis
 #########################################################################################################################
 
-# Summary
-boxplot(scale(df.2d[, 3:9]))
-summary(df.2d[, 3:9])
+# Table 1. Descriptive statistics
+df.aggr <- cbind(df.2d[, c(id.x) + 1],
+                 df.2d[, c(id.z, id.y[1]) + 1] * 10^-6,
+                 df.2d[, c(id.y[2]) + 1, drop = F],
+                 Total.Budget = rep(df.Z.0 * 10^-6, 5))
+
+table.1 <- sapply(df.aggr, function(x) c(Min  = min(x), 
+                                         Med  = median(x), 
+                                         Mean = mean(x), 
+                                         Max  = max(x), 
+                                         Std  = sd(x)))
+
+print(noquote(format(round(t(table.1), 2), big.mark = ",")))
+
+
+# Table 2. Comparative results of efficiency
+res.it <- dm.dea.intertemporal(df.3d[, id.x, ], df.3d[, id.y, ], df.3d[, id.z, ], df.Z.T, rts, ori)
+res.ba <- dm.dynamic.ba(df.3d[, id.x, ], df.3d[, id.y, ], df.3d[, id.z, ], df.Z.0, rts, ori)
+
+table.2 <- matrix(c(res.it$eff, res.ba$eff, res.it$eff.t, res.ba$eff.t), nrow(df.3d[,,1]), 
+                  dimnames = list(unique(df.2d$DMU), 
+                                  c("it.aggr", "ba.aggr", paste0("it.", 2012:2016), paste0("ba.", 2012:2016))))
+
+print(table.2[, c(1, 2, 3, 8, 4, 9, 5, 10, 6, 11, 7, 12)])
 
 # Table.1
 table.1 <- sapply(df.2d[, c(id.x, id.z, id.y) + 1], function(x) c(Min  = min(x), 
@@ -43,36 +63,9 @@ table.1 <- sapply(df.2d[, c(id.x, id.z, id.y) + 1], function(x) c(Min  = min(x),
                                                                   Std  = sd(x)))
 print(noquote(format(round(t(table.1), 2), big.mark = ",")))
 
-#########################################################################################################################
-### Analysis
-#########################################################################################################################
+# How many system efficient DMUs?
+apply(table.2[, 1:2], 2, function(x) sum(round(x, 8) == 1))
 
-# Run function
-res.it <- dm.dea.intertemporal(df.eff[, id.x, ], df.eff[, id.y, ], df.eff[, id.z, ], df.Z.T, rts, ori)
-res.ba <- dm.dynamic.ba(df.eff[, id.x, ], df.eff[, id.y, ], df.eff[, id.z, ], df.Z.0, rts, ori)
-res.pw <- cbind(dm.dea(df.eff[, c(id.x, id.z), 1], df.eff[, id.y, 1], rts, ori)$eff,
-                dm.dea(df.eff[, c(id.x, id.z), 2], df.eff[, id.y, 2], rts, ori)$eff,
-                dm.dea(df.eff[, c(id.x, id.z), 3], df.eff[, id.y, 3], rts, ori)$eff,
-                dm.dea(df.eff[, c(id.x, id.z), 4], df.eff[, id.y, 4], rts, ori)$eff,
-                dm.dea(df.eff[, c(id.x, id.z), 5], df.eff[, id.y, 5], rts, ori)$eff)
-
-# Compare results
-matrix(c(res.it$eff.t, res.ba$eff.t, res.pw), nrow(df.eff[,,1]), 
-       dimnames = list(unique(df.2d$DMU), 
-                       c(paste0("it.", 2012:2016), paste0("ba.", 2012:2016), paste0("in.", 2012:2016))))
-
-# Table.2
-table.2 <- data.frame(Name    = unique(df.2d$DMU), 
-                      it.avg  = round(res.it$eff,        4),
-                      ba.avg  = round(res.ba$eff,        4),
-                      it.2012 = round(res.it$eff.t[, 1], 4), 
-                      ba.2012 = round(res.ba$eff.t[, 1], 4), 
-                      it.2013 = round(res.it$eff.t[, 2], 4), 
-                      ba.2013 = round(res.ba$eff.t[, 2], 4), 
-                      it.2014 = round(res.it$eff.t[, 3], 4), 
-                      ba.2014 = round(res.ba$eff.t[, 3], 4), 
-                      it.2015 = round(res.it$eff.t[, 4], 4), 
-                      ba.2015 = round(res.ba$eff.t[, 4], 4), 
-                      it.2016 = round(res.it$eff.t[, 5], 4), 
-                      ba.2016 = round(res.ba$eff.t[, 5], 4))
-print(table.2)
+# Budget available at each T
+df.A.t <- array(df.Z.0, dim(df.3d)[c(1, 3)])
+for(i in 2:dim(df.3d)[3]){df.A.t[, i] <- df.A.t[, i - 1] - df.3d[, id.z, (i - 1), drop = F]}
